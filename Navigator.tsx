81c4db7a1c40998c0e75a0baea392ac02b81ef4f
Gabriel Abubakar <gabrielogrima@gmail.com>
2026-03-09 09:33:56 +0100

    feat: initial setup for deep linking

--- Navigator.tsx
+++ Navigator.tsx
@@ -1,16 +1,48 @@
 import { AuthGroup, DrawerGroup } from "@/components/navigation";
 import { RootStackParamList } from "@/types/stacks";
-import { NavigationContainer } from "@react-navigation/native";
+import { LinkingOptions, NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as Linking from "expo-linking";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { IndexScreen } from "./screens";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
+const linking: LinkingOptions<RootStackParamList> = {
+  prefixes: [
+    Linking.createURL("/"),
+    "https://app.example.com",
+    // "groceryaonboard://", // Or `Linking.createURL('/')` for Expo apps
+  ],
+  config: {
+    screens: {
+      Index: "",
+      DrawerGroup: {
+        screens: {
+          HomeTabGroup: {
+            screens: {
+              Home: "home",
+              Cart: "cart",
+              Favorite: "favorite",
+              Profile: "profile",
+            },
+          },
+        },
+      },
+      AuthGroup: {
+        screens: {
+          Login: "login",
+          Register: "register",
+        },
+      },
+    },
+  },
+};
+
 export default function Navigator() {
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={linking}>
       <StatusBar style="dark" />
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Index" component={IndexScreen} />
